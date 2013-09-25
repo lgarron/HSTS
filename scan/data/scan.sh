71207734a19e622847ed318cdbf6e6b7d20bda03
Lucas Garron <lucasg@gmx.de>
2013-09-25 00:59:53 -0700

    Restore Chrome user agent.

--- scan/data/scan.sh
+++ scan/data/scan.sh
@@ -4,6 +4,7 @@ mkdir -p "headers"
 mkdir -p "exclude"
 
 TOP_SITES_FILE="alexa-top-100k-2013-09-24.csv"
+CHROME_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.26"
 
 function query {
   URL_HERE="${1}"
@@ -12,19 +13,19 @@ function query {
   if [ -f "headers/${FILE}" ] ; then return ; fi
   if [ -f "exclude/${FILE}" ] ; then return ; fi
 
-  HEADERS=$(curl "--max-time" 20 -sI "${URL_HERE}")
+  HEADERS=$(curl --user-agent "${CHROME_USER_AGENT}" "--max-time" 20 -sI "${URL_HERE}")
   RETURN_CODE="${?}"
   if [ "${RETURN_CODE}" = "0" ]
   then
     echo -e "\033[92m[+] ${URL_HERE}\033[0m"
-    echo "${HEADERS}" > "headers/${FILE}"
+    echo -n "${HEADERS}" > "headers/${FILE}"
   elif [ "${RETURN_CODE}" = "143" ]
   then
     # Caused by `killall curl`.
     echo -e "\033[91m[-] Aborting ${URL_HERE} (error: ${RETURN_CODE})\033[0m"
   else
     echo -e "\033[91m[-] ${URL_HERE} (error: ${RETURN_CODE})\033[0m"
-    echo "${RETURN_CODE}" > "exclude/${FILE}"
+    echo -n "${RETURN_CODE}" > "exclude/${FILE}"
   fi
 }
 
@@ -44,10 +45,9 @@ function go {
   done
 }
 
-# GROUP_SIZE="50"
-GROUP_SIZE="2000"
+GROUP_SIZE="100"
 
-for i in $(seq ${GROUP_SIZE} ${GROUP_SIZE} 2000)
+for i in $(seq ${GROUP_SIZE} ${GROUP_SIZE} 10000)
 do
   # Take the last ${GROUP_SIZE} of the top ${i}.
   echo "${i}"
