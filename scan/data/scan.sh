72adfeb9194cd8521118a67b08ef8c9df06dcfaa
Lucas Garron <lucasg@gmx.de>
2013-09-25 10:40:02 -0700

    Top million.

--- scan/data/scan.sh
+++ scan/data/scan.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-mkdir -p "headers"
-mkdir -p "exclude"
+mkdir -p "results"
 
-TOP_SITES_FILE="alexa-top-100k-2013-09-24.csv"
+TOP_SITES_FILE="alexa-top-1m-2013-09-24.csv"
 CHROME_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.26"
 
 function query {
   URL_HERE="${1}"
   FILE="${2}"
-
-  if [ -f "headers/${FILE}" ] ; then return ; fi
-  if [ -f "exclude/${FILE}" ] ; then return ; fi
+  GROUP="${3}"
 
   HEADERS=$(curl --user-agent "${CHROME_USER_AGENT}" "--max-time" 20 -sI "${URL_HERE}" | grep -i "strict-transport-security")
   RETURN_CODE="${?}"
   if [ "${RETURN_CODE}" = "0" ]
   then
     echo -e "\033[92m[+] ${URL_HERE}\033[0m"
-    echo "${URL_HERE}" >> "secures.txt"
-  elif [ "${RETURN_CODE}" = "143" ]
-  then
-    # Caused by `killall curl`.
-    echo -e "\033[91m[-] Aborting ${URL_HERE} (error: ${RETURN_CODE})\033[0m"
-    echo "${URL_HERE}" >> "fails.txt"
+    echo "${URL_HERE}" >> "results/secures-${GROUP}.txt"
   else
     echo -e "\033[91m[-] ${URL_HERE} (error: ${RETURN_CODE})\033[0m"
-    echo "${URL_HERE}" >> "non-secures.txt"
+    echo "${URL_HERE}" >> "results/non-secures-${GROUP}.txt"
   fi
 }
 
@@ -39,16 +31,16 @@ function go {
     if [[ "${URL}" == *"/"* ]] ; then return ; fi
 
     echo "[${2}] ${URL}"
-    query "http://${URL}" "http-${URL}.txt"
-    query "http://www.${URL}" "http-www.${URL}.txt"
-    query "https://${URL}" "https-${URL}.txt"
-    query "https://www.${URL}" "https-www.${URL}.txt"
+    query "http://${URL}" "http-${URL}.txt" "${2}"
+    query "http://www.${URL}" "http-www.${URL}.txt" "${2}"
+    query "https://${URL}" "https-${URL}.txt" "${2}"
+    query "https://www.${URL}" "https-www.${URL}.txt" "${2}"
   done
 }
 
-GROUP_SIZE="500"
+GROUP_SIZE="5000"
 
-for i in $(seq ${GROUP_SIZE} ${GROUP_SIZE} 100000)
+for i in $(seq ${GROUP_SIZE} ${GROUP_SIZE} 1000000)
 do
   # Take the last ${GROUP_SIZE} of the top ${i}.
   echo "${i}"
