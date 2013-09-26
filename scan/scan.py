1b616a6c642c5f42b5395120d7a1a4b756599cd1
Lucas Garron <lucasg@gmx.de>
2013-09-26 12:32:45 -0700

    Starting to classify sites.

--- scan/scan.py
+++ scan/scan.py
@@ -4,6 +4,11 @@ from __future__ import print_function
 import csv
 import requests
 import requests_cache
+import re
+
+from itertools import groupby
+
+from collections import OrderedDict
 
 import shelve
 
@@ -16,42 +21,73 @@ requests_cache.install_cache(
 
 excludes = shelve.open("data/excludes")
 
+print_success = False
+
 def load(url):
 
-  print("\n###", url)
+  # print("\n###", url)
 
   if url in excludes:
-    print("[skipping]", url)
-    return
+    if print_success: print("[skipping]", url)
+    return (None, excludes[url])
 
   headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36"
   }
 
   try:
-    r = requests.get(url, allow_redirects=False, headers=headers, timeout=3)
-    print("[success]", url)
-  except:
-    excludes[url] = "exclude"
-    print("[adding to excludes]", url)
-    return
+    r = requests.get(url, headers=headers, timeout=10)
+    if print_success: print("[success]", url)
+  except Exception as e:
+    # TODO: if failed, try only one-hop
+    exceptionName = type(e).__name__
+    excludes[url] = exceptionName
+    if print_success: print("[adding to excludes]", url, "[", exceptionName, "]")
+    return (None, exceptionName)
 
-  print("status:", r.status_code)
+  return (r, None)
 
-  for key in ["strict-transport-security",
-              "location",
-              "pragma",
-              "cache-control"]:
-    try:
-      print(key + ":", r.headers[key])
-    except:
-      pass
+prefixes = OrderedDict()
+prefixes["http://"] = "h"
+prefixes["http://www."] = "hw"
+prefixes["https://"] = "s"
+prefixes["https://www."] = "sw"
 
-def go(base_url):
-  for protocol in ["http", "https"]:
-    for subdomain in ["www.", ""]:
-      load(protocol + "://" + subdomain + base_url)
 
+def canonicalize(r, domain):
+  s = re.sub(domain + ".*", "", r.url)
+
+  hsts = ""
+  if s not in ["http://", "http://www."]:
+    if "strict-transport-security" in r.headers:
+      hsts = "+"
+    else:
+      hsts = "-"
+
+  if s in prefixes:
+    return hsts + prefixes[s]
+  else:
+    return hsts + r.url
+
+
+def go(domain):
+  chains = []
+  for prefix in prefixes.keys():
+    r, e = load(prefix + domain)
+    if hasattr(r, "status_code"):
+      chain = list(r.history) + [r]
+
+      canon = [canonicalize(h, domain) for h in chain]
+      # Remove redundant redirects (from an HSTS perspective):
+      canon = [i[0] for i in groupby(canon)]
+
+      chains.append("[" + (" ".join(canon)) + "]")
+    else:
+      # chains.append("[" + e + "]")
+      chains.append("[X]")
+  print("".join(chains) + "," + domain)
+
+inFile = open("data/hsts_list_test.csv", "r")
 inFile = open("data/hsts_list.csv", "r")
 reader = csv.reader(inFile)
 
