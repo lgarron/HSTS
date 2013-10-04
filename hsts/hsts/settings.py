05f8dc94f75c3ca405d9b19aeae6e09a8ea62dd0
Lucas Garron <lucasg@gmx.de>
2013-10-04 03:12:06 -0700

    Scrapy code.

---
+++ hsts/hsts/settings.py
@@ -0,0 +1,20 @@
+# Scrapy settings for hsts project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'hsts'
+
+SPIDER_MODULES = ['hsts.spiders']
+NEWSPIDER_MODULE = 'hsts.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+CONCURRENT_REQUESTS = 20
+# LOG_LEVEL = 'INFO'
+COOKIES_ENABLED = False
+DOWNLOAD_TIMEOUT = 15
+RETRY_ENABLED = False
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36"
---
+++ hsts/hsts/spiders/test.py
@@ -0,0 +1,65 @@
+from scrapy.spider import BaseSpider
+from hsts.items import HSTSItem
+import csv
+import re
+
+
+class MySpider(BaseSpider):
+    name = 'hsts'
+
+    def __init__(self,
+            url_file="../data/alexa-top-100k-2013-09-24.csv",
+            start=1,
+            num=1000,
+            *args, **kwargs):
+
+        super(MySpider, self).__init__(*args, **kwargs)
+
+        try:
+            self.start = int(start)
+        except:
+            pass
+
+        try:
+            self.num = int(num)
+        except:
+            pass
+
+        self.start_urls = self.get_start_urls(url_file)
+
+    def get_start_urls(self, url_file):
+        urls = []
+
+        self.regex = re.compile(r"^(https?://(www\.)?)([^/]*)(/.*)?")
+        self.indices = {}
+
+        inFile = open(url_file, "r")
+        reader = csv.reader(inFile)
+
+        for i in range(1, self.start):
+            print("Skipping.")
+            reader.next()
+
+        for i in range(self.num):
+            row = reader.next()
+            self.indices[row[1]] = row[0]
+            for protocol in ["http", "https"]:
+                for www in ["", "www."]:
+                    urls.append(protocol + "://" + www + row[1])
+
+        return urls
+
+    def parse(self, response):
+        item = HSTSItem()
+        item["status"] = response.status
+        item["hsts"] = response.headers.get("strict-transport-security", None)
+
+        urls = response.meta.get("redirect_urls", [])
+        urls.append(response.url)
+        item["url"] = self.regex.sub(r"\1\3", urls[0])
+        item["urls"] = urls
+
+        domain = self.regex.sub(r"\3", urls[0])
+        item["index"] = self.indices.get(domain, None)
+
+        return [item]
