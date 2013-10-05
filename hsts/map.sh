e5a64f556924cdd699fb75caa96efa1d1e7b9a1c
Lucas Garron <lucasg@gmx.de>
2013-10-04 20:21:22 -0700

    Large slow scan.

--- hsts/map.sh
+++ hsts/map.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-ssh corn02 "~/HSTS/HSTS/hsts/worker.sh 50 001" &
-ssh corn20 "~/HSTS/HSTS/hsts/worker.sh 50 051" &
-ssh corn21 "~/HSTS/HSTS/hsts/worker.sh 50 101" &
-ssh corn07 "~/HSTS/HSTS/hsts/worker.sh 50 151" &
-ssh corn08 "~/HSTS/HSTS/hsts/worker.sh 50 201" &
-ssh corn09 "~/HSTS/HSTS/hsts/worker.sh 50 251" &
-ssh corn25 "~/HSTS/HSTS/hsts/worker.sh 50 301" &
-ssh corn16 "~/HSTS/HSTS/hsts/worker.sh 50 351" &
-ssh corn24 "~/HSTS/HSTS/hsts/worker.sh 50 401" &
-ssh corn01 "~/HSTS/HSTS/hsts/worker.sh 50 451" &
-ssh corn30 "~/HSTS/HSTS/hsts/worker.sh 50 501" &
-ssh corn27 "~/HSTS/HSTS/hsts/worker.sh 50 551" &
-ssh corn19 "~/HSTS/HSTS/hsts/worker.sh 50 601" &
-ssh corn29 "~/HSTS/HSTS/hsts/worker.sh 50 651" &
-ssh corn23 "~/HSTS/HSTS/hsts/worker.sh 50 701" &
-ssh corn06 "~/HSTS/HSTS/hsts/worker.sh 50 751" &
-ssh corn26 "~/HSTS/HSTS/hsts/worker.sh 50 801" &
-ssh corn03 "~/HSTS/HSTS/hsts/worker.sh 50 851" &
-ssh corn04 "~/HSTS/HSTS/hsts/worker.sh 50 901" &
-ssh corn28 "~/HSTS/HSTS/hsts/worker.sh 50 951" &
+ssh corn24 "~/HSTS/HSTS/hsts/worker.sh 100000 000001" &
+ssh corn27 "~/HSTS/HSTS/hsts/worker.sh 100000 100001" &
+ssh corn23 "~/HSTS/HSTS/hsts/worker.sh 100000 200001" &
+ssh corn30 "~/HSTS/HSTS/hsts/worker.sh 100000 300001" &
+ssh corn29 "~/HSTS/HSTS/hsts/worker.sh 100000 400001" &
+ssh corn26 "~/HSTS/HSTS/hsts/worker.sh 100000 500001" &
+ssh corn28 "~/HSTS/HSTS/hsts/worker.sh 100000 600001" &
+ssh corn19 "~/HSTS/HSTS/hsts/worker.sh 100000 700001" &
+ssh corn03 "~/HSTS/HSTS/hsts/worker.sh 100000 800001" &
+ssh corn11 "~/HSTS/HSTS/hsts/worker.sh 100000 900001" &
