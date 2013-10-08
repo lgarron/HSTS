ce3ff833eb0d9fc0fbf1bff623280483f435146d
Lucas Garron <lucasg@gmx.de>
2013-10-08 04:37:26 -0700

    Current map.sh version.

--- hsts/map.sh
+++ hsts/map.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-ssh corn02 "~/HSTS/HSTS/hsts/worker.sh 50 001" &
-ssh corn20 "~/HSTS/HSTS/hsts/worker.sh 50 051" &
-ssh corn21 "~/HSTS/HSTS/hsts/worker.sh 50 101" &
-ssh corn07 "~/HSTS/HSTS/hsts/worker.sh 50 151" &
-ssh corn08 "~/HSTS/HSTS/hsts/worker.sh 50 201" &
-ssh corn09 "~/HSTS/HSTS/hsts/worker.sh 50 251" &
-ssh corn25 "~/HSTS/HSTS/hsts/worker.sh 50 301" &
-ssh corn16 "~/HSTS/HSTS/hsts/worker.sh 50 351" &
-ssh corn24 "~/HSTS/HSTS/hsts/worker.sh 50 401" &
-ssh corn01 "~/HSTS/HSTS/hsts/worker.sh 50 451" &
-ssh corn30 "~/HSTS/HSTS/hsts/worker.sh 50 501" &
-ssh corn27 "~/HSTS/HSTS/hsts/worker.sh 50 551" &
-ssh corn19 "~/HSTS/HSTS/hsts/worker.sh 50 601" &
-ssh corn29 "~/HSTS/HSTS/hsts/worker.sh 50 651" &
-ssh corn23 "~/HSTS/HSTS/hsts/worker.sh 50 701" &
-ssh corn06 "~/HSTS/HSTS/hsts/worker.sh 50 751" &
-ssh corn26 "~/HSTS/HSTS/hsts/worker.sh 50 801" &
-ssh corn03 "~/HSTS/HSTS/hsts/worker.sh 50 851" &
-ssh corn04 "~/HSTS/HSTS/hsts/worker.sh 50 901" &
-ssh corn28 "~/HSTS/HSTS/hsts/worker.sh 50 951" &
+ssh corn15 "~/HSTS/HSTS/hsts/worker.sh 10000 00001" &
+ssh corn29 "~/HSTS/HSTS/hsts/worker.sh 10000 10001" &
+ssh corn24 "~/HSTS/HSTS/hsts/worker.sh 10000 20001" &
+ssh corn17 "~/HSTS/HSTS/hsts/worker.sh 10000 30001" &
+ssh corn21 "~/HSTS/HSTS/hsts/worker.sh 10000 40001" &
+ssh corn28 "~/HSTS/HSTS/hsts/worker.sh 10000 50001" &
+ssh corn30 "~/HSTS/HSTS/hsts/worker.sh 10000 60001" &
+ssh corn25 "~/HSTS/HSTS/hsts/worker.sh 10000 70001" &
+ssh corn27 "~/HSTS/HSTS/hsts/worker.sh 10000 80001" &
+ssh corn11 "~/HSTS/HSTS/hsts/worker.sh 10000 90001" &
